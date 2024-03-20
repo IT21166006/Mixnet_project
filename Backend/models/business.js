fb3759ac471b8d05dbcf09b11aa3a86feb427717
EASHAN_WIJESINGHA <wijesinghaeashan@gmail.com>
2024-03-21 01:00:58 +0530

    Update business.js

--- Backend/models/business.js
+++ Backend/models/business.js
@@ -43,7 +43,7 @@ const businessSchema = new Schema(
         //},
     }
 )
-//create model to bidding
+//create model to bussiness
 const bussiness = mongoose.model("bussiness",businessSchema);
 
 //export module
