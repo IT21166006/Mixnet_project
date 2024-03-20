4d803976cd1af6898cbac72c2ac7e0ec3d86ded0
viraj992 <99419786+viraj992@users.noreply.github.com>
2024-03-21 02:28:05 +0530

    Update consult.js route
    person in to consultant

--- Backend/routes/consult.js
+++ Backend/routes/consult.js
@@ -1,5 +1,5 @@
 const router = require("express").Router();
-let person = require("../models/person")
+let consultant = require("../models/consultant")
 
 
 
@@ -7,37 +7,30 @@ let person = require("../models/person")
 //http://localhost:8070/person/add
 router.route("/add").post((req,res)=>{
 
-    const name = req.body.name; 
+    const username = req.body.username;
+    const address = req.body.address;
     const age = Number(req.body.age);
-    const mobilenumber = Number(req.body.mobilenumber);
-    const userName = req.body.userName;
     const email = req.body.email;
-    const password = req.body.password;
-    const RePassword = req.body.RePassword;
-    const vehicleType = req.body.vehicleType;
+    
 
-    if(password==RePassword){
+    
 
     // object create
-    const newPerson = new person({
-        name,  
+    const newConsultant = new consultant({
+        username,
+        address,  
         age,
-        mobilenumber,
-        userName,
-        email,
-        password,
-        RePassword,
-        vehicleType
+        email
 
     })
     
     
-    newPerson.save().then(()=>{ 
-        res.json("Person Added")
+    newConsultant.save().then(()=>{ 
+        res.json("consulatant Added")
     }).catch((err)=>{
         console.log(err);
     })   
-}
+
 })
 
 
@@ -45,8 +38,8 @@ router.route("/add").post((req,res)=>{
 //http://localhost:8070/person/display
 router.route("/display").get((req,res)=>{
 
-    person.find().then((person)=>{  
-        res.json(person)
+    consultant.find().then((consultant)=>{  
+        res.json(consultant)
     }).catch((err)=>{
         console.log(err)
     }) 
@@ -57,19 +50,17 @@ router.route("/display").get((req,res)=>{
 //http://localhost:8070/person/update/64316bbc02cc8c3d90d151ed
 router.route("/update/:id").put(async(req,res) =>{
     let userId = req.params.id; 
-    const{name, age, mobilenumber, userName, email, password, vehicleType} = req.body; 
+    const{name, address, age, email} = req.body; 
 
-    const updatePerson = { 
+    const updateConsultant = { 
         name,
+        address,
         age,
-        mobilenumber,
-        userName,
-        email,
-        password,
-        vehicleType
+        email
+        
     }
 
-    const update = await person.findByIdAndUpdate(userId,updatePerson).then(()=>{      
+    const update = await consultant.findByIdAndUpdate(userId,updateConsultant).then(()=>{      
         res.status(200).send({status:"User updated"})
     }).catch((err)=> {
         console.log(err);
@@ -84,8 +75,8 @@ router.route("/update/:id").put(async(req,res) =>{
 router.route("/delete/:id").delete(async(req,res) =>{
     let userID =req.params.id;
 
-    await person.findByIdAndDelete(userID).then(() =>{
-        res.status(200).send({status: "user deleted"});
+    await consultant.findByIdAndDelete(userID).then(() =>{
+        res.status(200).send({status: "consulatant deleted"});
     }).catch((err) =>{
         console.log(err.message);
         res.status(500).send({status: "Error with delete user",error:err.message});
@@ -96,8 +87,8 @@ router.route("/delete/:id").delete(async(req,res) =>{
 //http://localhost:8070/person/get/642eb41a1c28e72cb8cfad9c
 router.route("/get/:id").get(async (req,res) =>{
     let userID = req.params.id;
-    const user = await person.findById(userID).then((person)=>{
-       res.status(200).send({status: "user fetched", person}) 
+    const user = await consultant.findById(userID).then((consultant)=>{
+       res.status(200).send({status: "user fetched", consultant}) 
     }).catch((err)=>{
         console.log(err.message);
         res.status(500).send({status: "Error with get user",error: err.message});
@@ -109,36 +100,23 @@ router.route("/login").post(async(req,res) => {
 
     try{
         const email = req.body.email;
-        const password = req.body.password;
+        
         console.log(email)
 
-         const newperson = await person.findOne({email:email, password:password})
+         const newconsultant = await consultant.findOne({email:email})
         //  if(newperson){
         //    
         //  }else{
 
         //  }
-         console.log(newperson)
-         res.status(200).json({data: newperson, status:200});
+         console.log(newconsultant)
+         res.status(200).json({data: newconsultant, status:200});
     }catch(err){
         res.status(500).json({message: err.message});
     }
 })
 
-//search
-router.get('/search/:searchInput', async (req, res) => {
-    try {
-      const { searchInput } = req.params;
-      const users = await person.find({
-        $or: [
-          { name: { $regex: searchInput, $options: 'i' } },
-        ],
-      });
-      res.json(users);
-    } catch (err) {
-      res.status(500).json({ message: err.message });
-    }
-  })
+
 
 
 
