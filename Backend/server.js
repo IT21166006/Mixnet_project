2ee127d70286f7ae544dad98c9b4ff7497cf5cb6
viraj992 <99419786+viraj992@users.noreply.github.com>
2024-03-21 02:34:29 +0530

    Update server.js
    server

--- Backend/server.js
+++ Backend/server.js
@@ -25,38 +25,15 @@ const connection = mongoose.connection;
 connection.once("open", () => {
     console.log("Mongodb Connection Success!");
 })
-//Admin Routes
-const adminRouter = require("./routes/admin.js");
-app.use("/admin",adminRouter);
 
 
-//Bidding Routs
-const biddingRouter = require("./routes/bidding.js");
-app.use("/bidding",biddingRouter);
 
-//Stock Routes
-const stockRouter = require("./routes/stock.js");
-app.use("/stock",stockRouter);
-
-//Customer Routes
-const customerRouter = require("./routes/customer.js")
-app.use("/customer",customerRouter);
-
-//Farmer Routes
-const farmerRouter = require("./routes/farmers.js");
-app.use("/farmer", farmerRouter);
 
 //Delevery Routes
-const personRouter = require("./routes/persons.js");
-app.use("/person",personRouter);
+const consultantRouter = require("./routes/consultants.js");
+app.use("/consultant",consultantRouter);
 
-//Payment Rounts
-const paymentRouter = require("./routes/payments.js");
-app.use("/payment",paymentRouter);
 
-//offer Routes
-const offerRouter= require("./routes/offers.js");
-app.use("/offers",offerRouter) 
 
 app.listen(PORT, () => {
     console.log (`Server is up and running on port number: ${PORT}`)
