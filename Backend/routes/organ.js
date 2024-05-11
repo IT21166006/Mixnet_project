246ed1ba5cdb66047f57e8c46faa98f6b37c7f1d
uresh pinidiya <99754729+IT21158636@users.noreply.github.com>
2024-05-11 06:56:23 +0530

    b
    b

--- Backend/routes/organ.js
+++ Backend/routes/organ.js
@@ -1,23 +1,85 @@
 const router = require("express").Router();
 const Organization = require("../models/organ.js");
+const multer = require('multer');
+const path = require('path');
 
-// Register new organization
-router.post("/register", (req, res) => {
-  const { OrganName, oaddress, phoneNumber, oemail, aboutus } = req.body;
 
-  const newOrganization = new Organization({
-    
+
+// Multer storage configuration
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, path.join(__dirname, "../uploads")); // Destination folder for uploads
+  },
+  filename: function (req, file, cb) {
+    cb(null, Date.now() + "-" + file.originalname); // Filename in uploads folder
+  }
+});
+
+// Multer upload instance
+const upload = multer({ storage: storage });
+
+
+
+router.route("/register").post(upload.single("image"), (req, res) => {
+  const { OrganName,oaddress,phoneNumber,oemail,number,aboutus} = req.body;
+  const image = req.file.filename; // Get the filename of the uploaded image
+
+  const neworganizations = new Employee({
+    OrganName,
+    oaddress,
+    phoneNumber,
+    oemail,
+    number,
+    aboutus,
   });
 
-  newOrganization.save()
-    .then(() => {
-      res.json("Organization registered successfully");
+
+  neworganizations.save().then(() => {
+    res.json(" Added organizations");
+  }).catch((err) => {
+    console.error(err);
+  })
+})
+
+
+router.get("/", (req, res) => {
+  Employee.find()
+    .then((organizations) => {
+      res.json(organizations);
     })
     .catch((err) => {
       console.log(err);
-      res.status(500).json({ error: "Failed to register organization" });
+      res.status(500).json({ error: "Failed to retrieve organizations" });
     });
 });
+
+
+//update
+router.route("/update/:id").put(async (req, res) => {
+  let userId = req.params.id;
+  const { OrganName, oaddress, phoneNumber, oemail, aboutus } = req.body;
+  //user id must change
+  const Updateorganizations = {
+
+    OrganName,
+    oaddress,
+    phoneNumber,
+    oemail,
+    aboutus
+  
+  }
+
+  const update = await Employee.findByIdAndUpdate(userId, Updateorganizations)
+  .then(() => {
+
+    res.status(200).send({ status: "employee updated" })
+
+  }).catch(() => {
+    console.log(err);
+    res.status(500).send({ status: "error with updating data", error: err.massage });
+  })
+
+})
 
 // Read all organizations
 router.get("/all", (req, res) => {
